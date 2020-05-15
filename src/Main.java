9188f0e53bdd87934a19941f3bc9a1fdae0858eb
uedamegumi <uedamegumi@192.168.1.13>
2020-05-16 03:32:20 +0900

    renshu1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("好きな食べ物はチーズです。");
+		System.out.println("なぜなら何に入れてもまろやかになって美味しいからです。");
+		
+	}
+
+}
