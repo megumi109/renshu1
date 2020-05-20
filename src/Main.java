f261c7658719e241517d32854a960f44798defd5
minamishibuya <minamishibuya@192.168.3.10>
2020-05-21 03:14:56 +0900

    課題1-2

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("好きな食べ物はチーズです。");
 		System.out.println("なぜなら何に入れてもまろやかになって美味しいからです。");
-		
+		System.out.println("トッピングの定番ですね。私も好きです。");
 	}
 
 }
